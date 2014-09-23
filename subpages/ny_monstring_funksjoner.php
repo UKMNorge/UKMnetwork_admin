ff74ed3f7d1eef59e4f0a4925d7085308c06a048
Marius Mandal <mariusmandal@gmail.com>
2014-09-23 10:29:52 +0200

    debug

--- subpages/ny_monstring_funksjoner.php
+++ subpages/ny_monstring_funksjoner.php
@@ -233,7 +233,7 @@ function UKMA_SEASON_urlsafe($text) {
 }
 function UKMA_SEASON_urlsafe_non_charset($text) {
 	
-	#$text = SMAS_encoding($text);
+	$text = SMAS_encoding($text);
 	echo '<hr>';
 	var_dump( $text );
 	
