4e91c334c84c4dc87e4445dfdcba9fdd941b0e82
Marius Mandal <mariusmandal@gmail.com>
2014-09-23 11:42:29 +0200

    global wpdb i fil

--- subpages/password_sync.inc.php
+++ subpages/password_sync.inc.php
@@ -3,8 +3,7 @@ if( !isset($_GET['sync'] ) ) {
 	die('Denne modulen vil oppdatere alle wordpressbrukere med passord satt i klarteksttabellen. Bruk get-parameter ?sync=true for å starte synkronisering');
 }
 
-require_once('UKM/sql.class.php');
-
+global $wpdb;
 $brukere = $wpdb->get_results("SELECT * FROM `ukm_brukere`", OBJECT);
 
 echo '<h2>Synkroniserer brukere</h2>';
